b17c5371a611749d1ff308f89b17c14454ad6072
yuichi0113 <73334313+yuichi0113@users.noreply.github.com>
2021-05-04 16:20:53 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="ja">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>Kousei_page</title>
+</head>
+
+<body>
+    <!-- ナビゲーションバー -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <!-- 左寄せ -->
+        <ul class="nav navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="navbar-brand" href="#"><img src="./image/icon_113240_256.png" alt="profile.img"
+                        id="profile_img"></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </li>
+        </ul>
+        <ul class="nav justify-content-end">
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">プロフィール</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">独立カウンセリング</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">独立カウンセリング流れ</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">お問い合わせ</a>
+            </li>
+        </ul>
+    </nav>
+
+    <!-- ヘッダーコンテンツ -->
+    <img src="./image/coffee-1276778_1920.jpg" class="img-fluid" alt="Responsive image">
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
